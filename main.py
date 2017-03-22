e815de760f5b1323225f3f62010d64bee1b2958b
nicoluce <nicolas.p.luce@gmail.com>
2017-03-22 11:58:59 -0300

    Points restarted error fixed

--- main.py
+++ main.py
@@ -22,13 +22,15 @@ def handle(message):
 			on_user_lefts(BOT, message)):
 			return
 		if cm.saved_date < datetime.date.today():
+			reset_points(BOT, message)
 			cm.main_db.collection.update_one({'_id':1}, 
 					{'$currentDate': {'date': {'$type':'date'}}})
-			reset_points(BOT, message)
+			cm.saved_date = cm.main_db.get_document({'_id':1})['date'].date()
 		if isCommand(message):
 			commandHandler(BOT, message, getCommand(message), getCommandParameters(message))
 			return
 		elif (not is_private(message) and 
+			'text' in message and 
 			message['text'].startswith('+') and 
 			message['text'].split('+')[1].isdigit() and
 			'reply_to_message' in message):
